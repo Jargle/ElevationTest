798343a09244167fc10291f3562293475394fa9b
Eli Rising <eli.rising@zonarsystems.com>
2018-06-18 19:59:06 -0700

    Replaced with a Kotlin service.

---
+++ app/src/main/java/com/jargle/elevationservice/elevationservice/ElevationService.kt
@@ -0,0 +1,12 @@
+package com.jargle.elevationservice.elevationservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ElevationService : Service() {
+
+    override fun onBind(p0: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
