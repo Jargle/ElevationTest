472d89f7fb2ce7e370220a1f776889ef2d559ff1
Eli Rising <eli.rising@zonarsystems.com>
2018-06-19 10:16:04 -0700

    Work with background service. Now requests location on service!

--- app/src/main/java/com/jargle/elevationservice/MainActivity.kt
+++ app/src/main/java/com/jargle/elevationservice/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.jargle.elevationservice
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.IdRes
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Button
 import com.jargle.elevationservice.elevationservice.ElevationService
@@ -22,9 +26,28 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onClick(p0: View?) {
         val id = p0!!.id
         if (id == R.id.btn_start_service) {
-            startService(Intent(this, ElevationService::class.java))
-        }
 
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, Array(1){Manifest.permission.ACCESS_FINE_LOCATION}, 1)
+            } else {
+                startElevtionService()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            1 -> {
+                startElevtionService()
+            } else -> {
+
+            }
+        }
+    }
+
+    private fun startElevtionService() {
+        startService(Intent(this, ElevationService::class.java))
     }
 
     // Used to lazily bind the button.
--- app/src/main/java/com/jargle/elevationservice/elevationservice/ElevationService.kt
+++ app/src/main/java/com/jargle/elevationservice/elevationservice/ElevationService.kt
@@ -1,17 +1,57 @@
 package com.jargle.elevationservice.elevationservice
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import java.util.concurrent.TimeUnit
 
-class ElevationService : Service() {
+class ElevationService : Service(), LocationListener{
+
+    private var myLocation : Location? = null
+    private var locationManager: LocationManager? = null
+
+    private val LOCATION_UPDATE_TIME = TimeUnit.SECONDS.toMillis(3)
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
+    private val TAG: String = "ElevationService"
+
     override fun onCreate() {
         super.onCreate()
+        if (locationManager == null) {
+            locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        }
 
+        try {
+            locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    LOCATION_UPDATE_TIME, 0f, this)
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Fail to request location update", e)
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Network provider does not exist", e)
+        }
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+        myLocation = p0
+        Toast.makeText(this, "New elevation: " + myLocation?.altitude, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+    }
+
+    override fun onProviderDisabled(p0: String?) {
     }
 }
