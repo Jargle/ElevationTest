777944370c496a3d374c5d57ef2feb40926c4d30
Eli Rising <eli.rising@zonarsystems.com>
2018-06-19 16:46:34 -0700

    Added comments to service. Some formatting tweaks and reorganization.

--- app/src/main/java/com/jargle/elevationservice/elevationservice/ElevationService.kt
+++ app/src/main/java/com/jargle/elevationservice/elevationservice/ElevationService.kt
@@ -22,6 +22,7 @@ private const val TAG = "ElevationService"
 private val LOCATION_UPDATE_TIME = TimeUnit.SECONDS.toMillis(5)
 private val df = DecimalFormat()
 private val simpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.US)
+// No need to register this in the manifest, we explicitly send this to the service.
 private const val SHUTDOWN_ACTION = "com.jargle.elevationservice.shutdownservice"
 
 class ElevationService : Service(), LocationListener {
@@ -36,39 +37,18 @@ class ElevationService : Service(), LocationListener {
 
     override fun onBind(p0: Intent?): IBinder? = null
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
-        if (SHUTDOWN_ACTION == intent?.action ?: "") {
-            stopForeground(true)
-            stopSelf()
-        } else {
-
-            val notificationIntent = Intent(this, ElevationService::class.java)
-            notificationIntent.action = SHUTDOWN_ACTION
-            val pendingIntent = PendingIntent.getService(this, 0,
-                    notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-            notificationBuilder.setContentTitle("Elevation")
-                    .setTicker("Elevation - Click to stop service")
-                    .setContentText("Elevation loading")
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentIntent(pendingIntent)
-                    .setOngoing(true)
-
-            startForeground(100, notificationBuilder.build())
-        }
-        return START_STICKY
-    }
-
     override fun onCreate() {
         super.onCreate()
-        locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+        /* Instantiate the notification builder. We keep it around so that updating the text is
+         * easier later
+         */
         notificationBuilder =
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
                     @Suppress("DEPRECATION")
                     NotificationCompat.Builder(this)
                 } else {
+                    // Required for Android O and above, notification channels for apps
                     val channelId = "my_service"
                     val channelName = "My Background Service"
                     val chan = NotificationChannel(channelId, channelName,
@@ -80,6 +60,9 @@ class ElevationService : Service(), LocationListener {
                     NotificationCompat.Builder(this, channelId)
                 }
 
+        /* Set up the location manager for updates.
+         */
+        locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         try {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                     LOCATION_UPDATE_TIME, 0f, this)
@@ -90,16 +73,50 @@ class ElevationService : Service(), LocationListener {
         }
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        if (SHUTDOWN_ACTION == intent?.action ?: "") {
+            /* If it's the shutdown action from the pending intent notification click, stop self.
+             * stopForeground only stops it from being a foreground service, and doesn't destroy it
+             */
+            stopForeground(true)
+            stopSelf()
+        } else {
+
+            // Pending intent that is sent to this service when the notification is clicked
+            val notificationIntent = Intent(this, ElevationService::class.java)
+            notificationIntent.action = SHUTDOWN_ACTION
+            val pendingIntent = PendingIntent.getService(this, 0,
+                    notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            // Notification setup
+            notificationBuilder.setContentTitle("Elevation - Click to stop service")
+                    .setContentText("Elevation loading")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    // Pending Intent sent out when notification clicked
+                    .setContentIntent(pendingIntent)
+                    .setOngoing(true)
+
+            // Start the service in the foreground with the notification
+            startForeground(100, notificationBuilder.build())
+        }
+        return START_STICKY
+    }
+
     override fun onDestroy() {
         super.onDestroy()
+        // Remove updates from the location manager for good cleanup.
         locationManager.removeUpdates(this)
     }
 
     override fun onLocationChanged(p0: Location?) {
         myLocation = p0
+        /* Update the notification with the correct text format when we get an update from the
+         * system.
+         */
         val formattedElevation = df.format(myLocation?.altitude)
         val date = simpleDateFormat.format(Date(myLocation?.time ?: 0L))
-        notificationBuilder.setContentText("New elevation: $formattedElevation at $date")
+        notificationBuilder.setContentText("$formattedElevation at $date")
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(100, notificationBuilder.build())
     }
